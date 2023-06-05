80208020c2c489585401bf908a0904ee56f1c6ca
benlaptop25 <ben.taylor2573@gmail.com>
2023-06-05 18:46:39 +0100

    Program.cs cli

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
 ﻿
 // FactoryMethod.FactoryMethod.Execute();
-Builder.Builder.Execute();
+// Builder.Builder.Execute();
+
+void MainMenu()
+{
+    bool repeat = true;
+    while (repeat)
+    {
+        Console.WriteLine("0. Quit");
+        Console.WriteLine("1. Creational Patterns");
+        Console.WriteLine("2. Structural Patterns");
+        Console.WriteLine("3. Behavioural Patterns");
+
+        int choice = -1;
+        do
+        {
+            Console.Write("-->");
+            string? inp = Console.ReadLine();
+
+            bool invalidInp = !int.TryParse(inp, out choice);
+            if (invalidInp) {
+                choice = -1;
+            }
+
+        } while (choice < 0 || choice > 3);
+
+        if (choice == 0) {
+            repeat = false;
+        }
+    }
+}
+
+MainMenu();
